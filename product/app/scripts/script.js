ccbbe8e81346f29d2ccbb2f40a27e9d532adc4c8
db001 <dnbennett@hotmail.co.uk>
2017-05-21 22:52:34 +0100

    working on hamburger toggle

---
+++ product/app/scripts/script.js
@@ -0,0 +1,17 @@
+const burger = document.getElementById('hamburger');
+const links = document.getElementById('links');
+
+console.log(links);
+
+/*
+
+function burgerTransform() {
+  burger.classList.toggle('open');
+  if (burger.classList.contains('open')) {
+    links.style.display = 'flex';
+  } else {
+    links.style.display = 'none';    
+  }
+}
+
+*/
