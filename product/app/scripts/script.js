990a19b4063862f0b9a71d366e570c1fde5c183b
db001 <dnbennett@hotmail.co.uk>
2017-05-23 12:57:34 +0100

    Eslint rules changes to ES6, still working on dropdown menu

--- product/app/scripts/script.js
+++ product/app/scripts/script.js
@@ -1,17 +1,17 @@
 const burger = document.getElementById('hamburger');
 const links = document.getElementById('links');
 
-console.log(links);
-
-/*
+// console.log(links);
 
 function burgerTransform() {
   burger.classList.toggle('open');
   if (burger.classList.contains('open')) {
     links.style.display = 'flex';
+    links.style.flexDirection = 'column';
+
   } else {
     links.style.display = 'none';    
   }
 }
 
-*/
+burger.addEventListener('click', burgerTransform);
