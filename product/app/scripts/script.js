84a904e32254283d0655c51341adc996d68eb85e
db001 <dnbennett@hotmail.co.uk>
2017-05-25 23:33:25 +0100

    Working on JS scrollTo nav links

--- product/app/scripts/script.js
+++ product/app/scripts/script.js
@@ -1,7 +1,6 @@
 const burger = document.getElementById('hamburger');
 const links = document.getElementById('links');
-
-// console.log(links);
+const nav = document.getElementsByClassName('nav-link');
 
 function burgerTransform() {
   burger.classList.toggle('open');
@@ -13,14 +12,28 @@ function burgerTransform() {
   }
 }
 
-burger.addEventListener('click', burgerTransform);
 
-window.addEventListener('resize', onResize)
-
-function onResize(e) {
+function onResize() {
   if(window.innerWidth >= 800) {
     links.style.display = "flex";
     links.style.flexDirection = 'row';
-    burger.style.display = none;
+    burger.style.display = 'none';
   }
-};
+}
+
+function getTop() {
+  const target = this.dataset.link;
+  const scrollTarget = document.getElementById(target);
+  const rect = scrollTarget.getBoundingClientRect();
+  setTimeout(function() {
+    window.scrollTo(0, rect.top);
+  }, 1)  
+}
+
+burger.addEventListener('click', burgerTransform);
+window.addEventListener('resize', onResize);
+
+for (let i = 0; i < nav.length; i++) {
+  nav[i].addEventListener('click', getTop);
+}
+
